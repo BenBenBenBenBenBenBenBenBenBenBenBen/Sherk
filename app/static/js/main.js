b5354aaab5e749e6987f19f2d0315cc209778d0d
Your Name <you@example.com>
2020-10-29 09:11:26 +1300

    bitch

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -42,9 +42,27 @@ $(document).ready(function(){
 		var image = image_data.image;
 		var description = `<p>${image.description}</p>`;
 		var title = `<h5 class="modal-title">${image.name}<i class="fa fa-times" data-dismiss="modal" aria-label="Close" aria-hidden="true"></i></h5>`;
-		var img = `<img src="${image.upload_location}" alt="${image.name}">`;
+        var img = `<figure class="filter-${image.filter}">
+                        <img class="modal-img" src="${image.upload_location}" alt="${image.name},${image.description}">
+                   </figure>`;
 		$('#image-modal .modal-body').html(img + title + description);
 		$('.modal').modal('show');
-	});
-
+    });
+    
+    
+    if ($('#filter-select').length > 0 ) {
+        var filter = $('#filter-select').data('filter');
+        $('#filter-select').val(filter);
+    }
+    
+    if ($('#category').length > 0 ) {
+        var category = $('#category').data('category');
+        $('#category').val(category);
+    }
+    
+    $('#filter-select').change(function(e) {
+        var new_filter = 'filter-' + this.value;
+        $('#image figure').removeClass();
+        $('#image figure').addClass(new_filter);
+    });
 });
